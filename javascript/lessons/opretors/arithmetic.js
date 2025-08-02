92adbb6324837b605c0b9c6ace71a4429b10de75
aditipittulwar <aditipittulwar@gmail.com>
2025-08-02 16:37:48 +0530

    added changes

---
+++ javascript/lessons/opretors/arithmetic.js
@@ -0,0 +1,9 @@
+
+let a = 40
+let b = 25
+
+console.log("addition of a and b",a+b)
+console.log("subtraction",a-b)
+console.log("Multiplication",a*b)
+console.log("Divi",a/b)
+console.log("Moduls",a%b)
